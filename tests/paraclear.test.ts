99ce5ca5e32aab0612d9b4e627cd9e57b4b04514
Lucca Mordente <106093+luccamordente@users.noreply.github.com>
2024-05-16 16:00:06 -0300

    Add getReceivableAmount and getSocializedLossFactor functions
    This is in preparation for being able to withdraw funds from Paradex.
    
    * `getSocializedLossFactor`: Get the current socialized loss factor from
      the Paraclear contract.
    * `getReceivableAmount`: Given a withdrawal amount, calculate the final
      amount that will be received depending on the socialized loss factor.

--- tests/paraclear.test.ts
+++ tests/paraclear.test.ts
@@ -1,4 +1,8 @@
-import { getTokenBalance } from '../src/paraclear';
+import {
+  getReceivableAmount,
+  getSocializedLossFactor,
+  getTokenBalance,
+} from '../src/paraclear';
 
 import { configFactory } from './factories/paradex-config';
 
@@ -57,5 +61,111 @@ describe('getBalance', () => {
     });
 
     await expect(result).rejects.toThrow('Failed to parse token balance');
+  });
+});
+
+describe('getSocializedLossFactor', () => {
+  test('should return the socialized loss factor', async () => {
+    const mockProvider = { callContract: jest.fn() };
+    mockProvider.callContract.mockResolvedValueOnce(['123456789']);
+
+    const result = await getSocializedLossFactor({
+      config: configFactory(),
+      provider: mockProvider,
+    });
+
+    expect(result).toEqual({ socializedLossFactor: '1.23456789' });
+  });
+
+  test('should throw an error if the result is null', async () => {
+    const mockProvider = { callContract: jest.fn() };
+    mockProvider.callContract.mockResolvedValueOnce([null]);
+
+    const result = getSocializedLossFactor({
+      config: configFactory(),
+      provider: mockProvider,
+    });
+
+    await expect(result).rejects.toThrow(
+      'Failed to get socialized loss factor',
+    );
+  });
+
+  test('should throw an error if the result is not a number', async () => {
+    const mockProvider = { callContract: jest.fn() };
+    mockProvider.callContract.mockResolvedValueOnce(['not a number']);
+
+    const result = getSocializedLossFactor({
+      config: configFactory(),
+      provider: mockProvider,
+    });
+
+    await expect(result).rejects.toThrow(
+      'Failed to parse socialized loss factor',
+    );
+  });
+});
+
+describe('getReceivableAmount', () => {
+  test('should calculate the receivable amount correctly', async () => {
+    const socializedLossFactor = '0.01';
+    const socializedLossFactorChain = '1000000';
+
+    const mockProvider = { callContract: jest.fn() };
+    mockProvider.callContract.mockResolvedValueOnce([
+      socializedLossFactorChain,
+    ]);
+
+    const result = await getReceivableAmount({
+      config: configFactory(),
+      provider: mockProvider,
+      amount: '100',
+    });
+
+    expect(result).toStrictEqual({
+      receivableAmount: '99',
+      receivableAmountChain: '9900000000',
+      socializedLossFactor,
+    });
+  });
+
+  test('should throw an error for invalid amount', async () => {
+    const result = getReceivableAmount({
+      config: configFactory(),
+      provider: { callContract: jest.fn() },
+      amount: 'not a number',
+    });
+
+    await expect(result).rejects.toThrow('Invalid amount');
+  });
+
+  test('should throw an error if socialized loss factor is not a number', async () => {
+    const mockProvider = { callContract: jest.fn() };
+    mockProvider.callContract.mockResolvedValueOnce(['not a number']);
+
+    const result = getReceivableAmount({
+      config: configFactory(),
+      provider: mockProvider,
+      amount: '100',
+    });
+
+    await expect(result).rejects.toThrow(
+      'Failed to parse socialized loss factor',
+    );
+  });
+
+  test('should throw an error if socialized loss factor is null', async () => {
+    const mockProvider = { callContract: jest.fn() };
+    mockProvider.callContract.mockResolvedValueOnce([null]);
+
+    const result = getReceivableAmount({
+      config: configFactory(),
+      provider: mockProvider,
+      amount: '100',
+    });
+
+    await expect(result).rejects.toThrow(
+      'Failed to get socialized loss factor',
+    );
   });
 });
